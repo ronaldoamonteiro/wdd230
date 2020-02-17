d67de9d2108534340d7ee6fd2c1befa6e8813cd2
ronaldoamonteiro <ron.monteiro07@gmail.com>
2020-02-17 15:11:56 -0700

    wind chill

---
+++ lesson6/js/windchill.js
@@ -0,0 +1,4 @@
+const t = parseFloat(document.getElementById('high').textContent);
+const s = parseFloat(document.getElementById('windspeed').textContent);
+const f = 35.74 + (0.6215*t) - 35.75*(Math.pow(s,0.16)) + 0.4275*t*(Math.pow(s,0.16));
+document.getElementById('windchill').textContent = f.toFixed(1);

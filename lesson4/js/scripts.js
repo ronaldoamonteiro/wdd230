c6d047ba6892a3f02e4f8acbe0f749b432869b3a
ronaldoamonteiro <ron.monteiro07@gmail.com>
2021-01-29 18:51:13 -0300

    last

--- lesson4/js/scripts.js
+++ lesson4/js/scripts.js
@@ -1,3 +1,10 @@
 function toglleMenu(){
     document.getElementById("primaryNav").classList.toggle("hide");
+    if (document.querySelector('button').textContent != "X"){
+        document.querySelector('button').textContent = "X";
+    }
+    else{
+        document.querySelector('button').textContent = '|||';
+    }
+    
 }

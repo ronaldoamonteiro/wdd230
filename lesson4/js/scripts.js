6913d43184e40f0843451153a607040c0268c295
ronaldoamonteiro <ron.monteiro07@gmail.com>
2021-05-13 07:36:35 -0400

    humburguer - ajustments

--- lesson4/js/scripts.js
+++ lesson4/js/scripts.js
@@ -1,10 +1,11 @@
 function toglleMenu(){
     document.getElementById("primaryNav").classList.toggle("hide");
-    if (document.querySelector('button').textContent != "X"){
+    document.getElementById("iconx").classList.toggle("rotate");
+    if (document.querySelector('button').textContent == "|||"){
         document.querySelector('button').textContent = "X";
     }
     else{
-        document.querySelector('button').textContent = '|||';
+        document.querySelector('button').innerText = "|||";
     }
     
 }

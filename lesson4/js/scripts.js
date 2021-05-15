c81997942ca60914511142f04cc6c01930e84147
ronaldoamonteiro <ron.monteiro07@gmail.com>
2021-05-15 19:01:43 -0400

    Lesson 4 - preston page

--- lesson4/js/scripts.js
+++ lesson4/js/scripts.js
@@ -1,11 +1,3 @@
 function toglleMenu(){
     document.getElementById("primaryNav").classList.toggle("hide");
-    document.getElementById("iconx").classList.toggle("rotate");
-    if (document.querySelector('button').textContent == "|||"){
-        document.querySelector('button').textContent = "X";
-    }
-    else{
-        document.querySelector('button').innerText = "|||";
-    }
-    
 }

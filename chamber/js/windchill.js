afe5c81baa9a490560b725f9a3ac53e533a07924
ronaldoamonteiro <ron.monteiro07@gmail.com>
2022-04-23 15:22:29 -0600

    new semester

---
+++ chamber/js/windchill.js
@@ -0,0 +1,7 @@
+const t = parseFloat(document.getElementById('high').textContent);
+const s = parseFloat(document.getElementById('windspeed').textContent);
+
+if (t<=50 && s>4.8){
+const f = 35.74 + (0.6215*t) - 35.75*(Math.pow(s,0.16)) + 0.4275*t*(Math.pow(s,0.16));
+document.getElementById('windchill').textContent = f.toFixed(1);
+}
